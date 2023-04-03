4fdd4372dac0fc2155ac565b289f22cbe48643a0
corentin crepin <corentin62.crepin@gmail.com>
2023-04-03 18:52:44 +0200

    Ajout de la commande urban

---
+++ tp4/src/commands/urban.js
@@ -0,0 +1,18 @@
+// créez une commande 'urban' qui utilise l'API urbandictionary (https://api.urbandictionary.com/v0/define? + votre terme à chercher
+
+const { SlashCommandBuilder } = require('discord.js')
+const { request } = require('undici')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('urban')
+        .setDescription('Renvoie une définition de votre terme.')
+        .addStringOption(option => option.setName('input').setDescription('Input de l\'utilisateur').setRequired(true)),
+    async execute(interaction) {
+        const term = interaction.options.getString('input')
+        const urban = await request(`https://api.urbandictionary.com/v0/define?term=${term}`)
+        const { list } = await urban.body.json()
+        const { definition } = list[0]
+        await interaction.reply(definition)
+    }
+}
