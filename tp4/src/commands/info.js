3779258c7c7bb21b656ee368d7f0be7a9e62fef0
corentin crepin <corentin62.crepin@gmail.com>
2023-04-03 18:37:20 +0200

    Ajout de la commande info

---
+++ tp4/src/commands/info.js
@@ -0,0 +1,33 @@
+// commande avec sous-commande 'user' et 'server', si server est sélectionné sort les mêmes informations que la commande server, si user sort les mêmes infos que la commande user (si un pseudo est passé avec la sous-commande, sort les informations sur l'utilisateur passé en option, sinon sur l'utilisateur commanditaire)
+
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Réponse : Nom de l\'utilisateur et date d\'arrivée sur le serveur!')
+        .addSubcommand(subcommand => subcommand
+            .setName('user')
+            .setDescription('Réponse : Nom de l\'utilisateur et date d\'arrivée sur le serveur!')
+            .addStringOption(option => option
+                .setName('input')
+                .setDescription('Input de l\'utilisateur')))
+        .addSubcommand(subcommand => subcommand
+            .setName('server')
+            .setDescription('Réponse : Nom du serveur et nombre de membres!')),
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+        if (subcommand === 'user') {
+            const user = interaction.options.getString('input');
+            if (user) {
+                await interaction.reply(`Nom de l'utilisateur: ${user}\nDate d'arrivée sur le serveur: ${interaction.member.joinedAt}`);
+            } else {
+                await interaction.reply(`Nom de l'utilisateur: ${interaction.user.username}\nDate d'arrivée sur le serveur: ${interaction.member.joinedAt}`);
+            }
+        } else if (subcommand === 'server') {
+            await interaction.reply(`Nom du serveur: ${interaction.guild.name}\nNombre de membres: ${interaction.guild.memberCount}`);
+        }
+    }
+}
+
